588f6e701893133a812dd49cca9c5a1c1ea07d11
Brayan Muniz <brayanMuniz2636@gmail.com>
2022-07-19 18:26:47 -0500

    Reads file line by line

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import asyncio
 import requests
 from bs4 import BeautifulSoup
+kanji_url = "https://www.romajidesu.com/kanji/"
+file_name = "song-name.txt" 
+
+all_lines = []
+with open(file_name) as file:
+    all_lines = [line.rstrip() for line in file]
+
+# Remove duplicates
+lines = []
+[lines.append(x) for x in all_lines if x not in lines]
+print("Wait around", len(lines) + 5, "seconds")
+
+formatted_kanji = {}
 
 
 def minify_kanji_detail(kanji):
@@ -17,22 +31,29 @@ def minify_kanji_detail(kanji):
     return formatted
 
 
-# First line from Band-Maid song "Choose Me"
-r = requests.get('https://www.romajidesu.com/kanji/愛こそはね%20目に見えない幻想')
+async def get_kanji_from_line(line):
+    full_url = kanji_url + line.replace(" ", "%20")
+    r = requests.get(full_url)
 
-if r.status_code == 200:
-    formatted_kanji = []
-    soup = BeautifulSoup(r.content, "html.parser")
-    soup.prettify()
-    kanji_list = soup.find_all("div", class_="kanji_detail")
-    for kanji in kanji_list:
-        formatted_kanji.append(minify_kanji_detail(kanji))
+    if r.status_code == 200:
+        kanji_list = []
+        soup = BeautifulSoup(r.content, "html.parser")
+        soup.prettify()
+        kanji_detail = soup.find_all("div", class_="kanji_detail")
+        for kanji in kanji_detail:
+            kanji_list.append(minify_kanji_detail(kanji))
 
-    for kanji in formatted_kanji: 
-        print(kanji)
+        for kanji in kanji_list:
+            if(kanji["symbol"] not in formatted_kanji.keys()):
+                formatted_kanji[kanji["symbol"]] = kanji
+    await asyncio.sleep(.5)
 
-# RomajiDesu Html Template
-#   .kanji_detail
-#       .kanji_left,         contains: kanji, radicals
-#       .kanji_right,        contains: meanings
-#       .kanji_strokes_order contains: rect(s) inside of svg
+loop = asyncio.get_event_loop()
+tasks = []
+for line in lines:
+    tasks.append(get_kanji_from_line(line=line))
+
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+print(formatted_kanji)
